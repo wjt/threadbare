a5135af19e04fbb80e70b528d1f080500e4004e8
Will Thompson <wjt@endlessos.org>
2025-03-24 16:35:13 +0000

    music_puzzle: Simplify the bard playing the xylophone
    Previously, the communication between the bard dialogue and the
    xylophone was managed by using emotion tags in the dialogue, which were
    then detected by the bard's script and transformed into xylophone actions.
    This was a bit convoluted and also made it impossible to synchronise the
    notes with the dialogue text.
    
    Now that the Talker node (in this case the Bard) is passed to Dialogue
    Manager as extra game state, the dialogue can call methods on the bard.
    As a bonus, if the method is a coroutine (i.e. a function that uses
    `await`), typing of dialogue will pause until the coroutine resolves.
    
    Define a play() method on the bard, which is not necessary but makes the
    dialogue more readable. Call it from the dialogue in place of using
    tags. Remove the special handling of dialogue tags.
    
    https://github.com/nathanhoad/godot_dialogue_manager/blob/main/docs/Conditions_Mutations.md#mutations

--- scenes/music_puzzle/bard.gd
+++ scenes/music_puzzle/bard.gd
@@ -3,22 +3,8 @@
 class_name Bard
 extends Talker
 
-@export var xylophone: MusicalRockXylophone
+@export var puzzle: MusicPuzzle
 
 
-func _on_interaction_started() -> void:
-	DialogueManager.got_dialogue.connect(_on_got_dialogue)
-	super()
-
-
-func _on_got_dialogue(line: DialogueLine) -> void:
-	# TODO: This is kind of an abuse of the tag system. It would be better to
-	# use mutations so the dialogue waits for the play_note() method to finish.
-	for tag: String in line.tags:
-		if tag.begins_with("note-"):
-			await xylophone.play_note(tag.right(-"note-".length()))
-
-
-func _on_dialogue_ended(dialogue_resource: DialogueResource) -> void:
-	DialogueManager.got_dialogue.disconnect(_on_got_dialogue)
-	super(dialogue_resource)
+func play(note: String) -> void:
+	await xylophone.play_note(note)
