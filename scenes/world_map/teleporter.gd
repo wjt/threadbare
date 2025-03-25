d97ff5a3b6ab5b7a4f6b58d8d2dd855bbdc1e486
pablitar <pablitar@gmail.com>
2025-03-25 13:01:08 -0300

    Adding a spawn point for teleporters
    Since areas in the world map could have different entrances, it's important that a teleporter has information not only to which scene it should go to, but also to which point on that scene the player should spawn.
    
    This PR adds a property called "spawn_point" to each teleporter, which can contain the node path of the spawn point in which the player should be moved at the start of the scene. Some considerations:
    - Since the node reference of the spawn point is to another scene, we cannot use the default behaviour of the @export annotation with a NodePath. Instead, we can read the target scene and find all those nodes that belong to the "spawn_point" group, and list those as options. This approach work quite well, with some limitations. For instance, it doesn't work if the spawn point is defined in the target scene, but instead in a instanced scene (scene within a scene)
    - To identify the player, a group called "player" was added
    - To encapsulate the logic of changing scenes, a SceneSwitcher singleton was added. For the time being it is quite simple, but eventually could have logic regarding transitions, scene initialization, caching, and may other goodies.
    - To communicate the spawn point to the target scene, we are using the GameState singleton. This is encapsulated in the SceneSwitcher singleton in case we want to change this implementation eventually.
    - To prevent camera smoothing from misbehaving when the player spawns somewhere, smoothing is temporarily disabled during the process of spawn. This could be followed up with an issue of implementing a proper camera with its own behavior and script, and maybe make the camera "teleport aware"

--- scenes/world_map/teleporter.gd
+++ scenes/world_map/teleporter.gd
@@ -1,17 +1,71 @@
+@tool
 class_name Teleporter
 extends Area2D
 
-@export_file("*.tscn") var scene_to_go_to: String
+const SPAWN_POINT_GROUP_NAME: String = "spawn_point"
+
+@export_file("*.tscn") var scene_to_go_to: String:
+	set(new_value):
+		scene_to_go_to = new_value
+		_update_available_spawn_points()
+		notify_property_list_changed()
+
+var spawn_point_path: NodePath:
+	set(new_val):
+		if new_val == ^"NONE":
+			spawn_point_path = ^""
+		else:
+			spawn_point_path = new_val
+
+var _available_spawn_points: Array[NodePath] = []
 
 
 func _ready() -> void:
-	self.body_entered.connect(_on_body_entered)
 	collision_layer = 0
 	collision_mask = 0
 	set_collision_layer_value(3, true)
 	set_collision_mask_value(1, true)
 
+	if Engine.is_editor_hint():
+		return
+
+	self.body_entered.connect(_on_body_entered)
+
 
 func _on_body_entered(_body: PhysicsBody2D) -> void:
 	if scene_to_go_to:
-		get_tree().change_scene_to_file(scene_to_go_to)
+		# We are using call_deferred here because removing nodes with
+		# collisions during a callback caused by a collision might cause
+		# undesired behavior.
+		SceneSwitcher.change_scene_to.call_deferred(scene_to_go_to, spawn_point_path)
+
+
+func _update_available_spawn_points() -> void:
+	if ResourceLoader.exists(scene_to_go_to, "PackedScene"):
+		var packed_scene: PackedScene = load(scene_to_go_to)
+		var paths: Array[NodePath] = []
+		var scene_state: SceneState = packed_scene.get_state()
+
+		for i: int in scene_state.get_node_count():
+			if SPAWN_POINT_GROUP_NAME in scene_state.get_node_groups(i):
+				var node_path_as_string: String = String(scene_state.get_node_path(i))
+
+				paths.push_back(NodePath(node_path_as_string.replace("./", "")))
+
+		_available_spawn_points = paths
+
+
+func _get_property_list() -> Array[Dictionary]:
+	var property_list: Array[Dictionary] = []
+
+	property_list.push_back(
+		{
+			"name": "spawn_point_path",
+			"type": TYPE_STRING,
+			"hint": PROPERTY_HINT_ENUM,
+			"hint_string": ",".join(["NONE"] + _available_spawn_points),
+			"usage": PROPERTY_USAGE_DEFAULT
+		}
+	)
+
+	return property_list
