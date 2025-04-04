667c9333d3211f894a7fa791fd074c73af98fbcf
Manuel Quiñones <manuel.por.aca@gmail.com>
2025-04-04 15:10:15 -0300

    Player fighting: Blow air in all directions
    Simplify the ink combat mechanic. The player now hits ink all around,
    without having to point up, down, left or right. Use a circle shape for
    the collision. Leave a single "blow" animation and  that has the circle
    shape growing radius. Make this a looping animation.

--- scenes/player/animation_player.gd
+++ scenes/player/animation_player.gd
@@ -2,7 +2,6 @@
 # SPDX-License-Identifier: MPL-2.0
 extends AnimationPlayer
 
-const FIGHT_ANIMATIONS: Array[StringName] = [&"blow_right", &"blow_left", &"blow_up", &"blow_down"]
 var cancellable: bool = false
 
 @onready var player: Player = owner
@@ -10,7 +9,7 @@ var cancellable: bool = false
 
 
 func _process(_delta: float) -> void:
-	if not player_fighting.is_fighting and current_animation not in FIGHT_ANIMATIONS:
+	if not player_fighting.is_fighting and current_animation != &"blow":
 		cancellable = false
 		_process_walk_idle(_delta)
 	else:
@@ -25,24 +24,12 @@ func _process_walk_idle(_delta: float) -> void:
 
 
 func _process_fighting(_delta: float) -> void:
-	if current_animation in FIGHT_ANIMATIONS and current_animation_position > 0.3:
+	if current_animation == &"blow" and current_animation_position > 0.3:
 		cancellable = true
 
 	if not player_fighting.is_fighting:
-		if (
-			cancellable
-			and current_animation in FIGHT_ANIMATIONS
-			and current_animation_position <= 0.3
-		):
+		if cancellable and current_animation == &"blow" and current_animation_position <= 0.3:
 			stop()
 		return
 
-	var angle: float = player.last_nonzero_axis.angle()
-	if abs(-PI / 2 - angle) <= PI / 4:
-		play(&"blow_up")
-	elif abs(PI / 2 - angle) <= PI / 4:
-		play(&"blow_down")
-	elif abs(PI - angle) <= PI / 4:
-		play(&"blow_left")
-	elif abs(angle) <= PI / 4:
-		play(&"blow_right")
+	play(&"blow")
