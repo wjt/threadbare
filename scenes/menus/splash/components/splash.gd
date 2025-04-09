a5b36dc208bfb119cdff7c836cd23a14964bd2cd
Juan Manuel Fernandes dos Santos <juan9794@gmail.com>
2025-04-09 11:17:56 -0300

    Reorganize project files
    Based on what was discussed in https://github.com/endlessm/threadbare/issues/75, the changes are:
    
    In scenes/, have the folders
    
     - story_quests, where we will store the playable levels that make up each story quest.
    
     - world_map, here will live the scenes that make up the open world of the game.
    
     - game_elements, reusable elements (enemies, npcs, props) that are the building blocks of any playable scene.
    
     - menues (pause, main_screen, credits, etc) are the scenes that aren't playable and also are mainly UI centric.
    
     - ui_elements, the building blocks of the menues scenes, and also extradiegetic elements like huds.
    
     - globals, nodes that live outside of any scene but are needed for the game to work.
    
    Also, another decision taken was to have the .tscn files apart from the other files that make up a scene (script files, assets, resources, etc). The idea behind this is that we want learners to be able to quickly figure out what's the 'ready to use' file that they can drag to the editor.

---
+++ scenes/menus/splash/components/splash.gd
@@ -0,0 +1,21 @@
+# SPDX-FileCopyrightText: The Threadbare Authors
+# SPDX-License-Identifier: MPL-2.0
+extends Node2D
+
+
+func _ready() -> void:
+	$LogoStitcher.finished.connect(_on_logo_stitcher_finished)
+
+
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed(&"ui_accept"):
+		switch_to_intro()
+
+
+func switch_to_intro() -> void:
+	SceneSwitcher.change_to_packed(preload("res://scenes/menus/intro/intro.tscn"))
+
+
+func _on_logo_stitcher_finished() -> void:
+	await get_tree().create_timer(5.0).timeout
+	switch_to_intro()
