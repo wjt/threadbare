1a4d0319bf77c738718528e7ba5909603028624a
Will Thompson <wjt@endlessos.org>
2025-04-14 15:51:37 +0100

    Splash: Skip on ui_cancel (Esc/B) as well as ui_accept (Space/A)

--- scenes/menus/splash/components/splash.gd
+++ scenes/menus/splash/components/splash.gd
@@ -14,7 +14,7 @@ func _ready() -> void:
 
 
 func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed(&"ui_accept"):
+	if Input.is_action_just_pressed(&"ui_accept") or Input.is_action_just_pressed(&"ui_cancel"):
 		switch_to_intro()
 
 
