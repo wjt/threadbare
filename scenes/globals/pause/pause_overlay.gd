1de77b7259e7af04163bf93d275b49e1ab854091
pablitar <pablitar@gmail.com>
2025-04-14 18:37:09 -0300

    Add Pause Overlay as a Global Scene
    A simple scene that pauses the game when the user presses escape. It's globally available, so it works in all scenes.
    
    Also: Changed the default font of the theme, so it can be used for labels.
    
    Fixes #170

---
+++ scenes/globals/pause/pause_overlay.gd
@@ -0,0 +1,18 @@
+# SPDX-FileCopyrightText: The Threadbare Authors
+# SPDX-License-Identifier: MPL-2.0
+extends CanvasLayer
+
+
+func _unhandled_key_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		toggle_pause()
+		get_viewport().set_input_as_handled()
+
+
+func toggle_pause() -> void:
+	if Pause.is_paused(Pause.System.GAME):
+		visible = false
+		Pause.unpause_system(Pause.System.GAME, self)
+	else:
+		visible = true
+		Pause.pause_system(Pause.System.GAME, self)
