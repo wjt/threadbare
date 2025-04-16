1a156223b08894161262d0a28eb7962aeb77acff
Tobías Romero <arebuayon@hotmail.com>
2025-04-16 12:21:12 -0300

    Change interaction to use an area
    Interaction area lets us detect collisions even if they arent exactly in front of the player
    
    This fixes https://github.com/endlessm/threadbare/issues/154

--- scenes/game_elements/characters/player/components/player_interaction.gd
+++ scenes/game_elements/characters/player/components/player_interaction.gd
@@ -6,33 +6,33 @@ extends Node2D
 var is_interacting: bool:
 	get = _get_is_interacting
 
-@onready var interact_ray: RayCast2D = %InteractRay
+@onready var interact_zone: Area2D = %InteractZone
 @onready var interact_marker: Marker2D = %InteractMarker
 @onready var interact_label: FixedSizeLabel = %InteractLabel
 
 
 func _get_is_interacting() -> bool:
-	return not interact_ray.enabled
+	return not interact_zone.monitoring
 
 
 func _process(_delta: float) -> void:
 	if is_interacting:
 		return
-	var interact_area: InteractArea = interact_ray.get_interact_area()
+	var interact_area: InteractArea = interact_zone.get_interact_area()
 
 	if %PlayerController.inputs_paused() or not interact_area:
 		interact_label.visible = false
 		return
 
 	if %PlayerController.is_action_just_pressed(&"ui_accept"):
-		interact_ray.enabled = false
+		interact_zone.monitoring = false
 		interact_label.visible = false
 		interact_area.interaction_ended.connect(_on_interaction_ended, CONNECT_ONE_SHOT)
-		interact_area.start_interaction(interact_ray.target_position.x < 0)
+		interact_area.start_interaction(interact_zone.is_looking_from_right)
 	else:
 		interact_label.visible = true
 		interact_label.label_text = interact_area.action
 
 
 func _on_interaction_ended() -> void:
-	interact_ray.enabled = true
+	interact_zone.monitoring = true
