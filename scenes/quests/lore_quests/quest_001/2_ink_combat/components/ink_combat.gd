e763777b45859d91ca1c1cdc50fbf59c431fe005
Juan Manuel Fernandes dos Santos <jfdossantos@frba.utn.edu.ar>
2025-04-21 11:07:04 -0300

    Add skeleton for ink combat rounds
    In the first 2 ink combat rounds, replaced the collectible by a door + a teleporter.
    
    Also, changed ink_combat.gd so it doesn't require a collectible and instead just emits a signal when it's complete.
    Finally, made it possible for an ink_combat.gd to not have a starting dialogue. If there's no dialogue, the challenge will start 0.5 seconds after the level is loaded, if there's dialogue, it will start 0.5 seconds after the dialogue ended.
    
    Fixes https://github.com/endlessm/threadbare/issues/241

--- scenes/quests/lore_quests/quest_001/2_ink_combat/components/ink_combat.gd
+++ scenes/quests/lore_quests/quest_001/2_ink_combat/components/ink_combat.gd
@@ -2,20 +2,25 @@
 # SPDX-License-Identifier: MPL-2.0
 extends Node2D
 
+signal goal_reached
+
 @export var intro_dialogue: DialogueResource
 
 @onready var fill_game_logic: FillGameLogic = %FillGameLogic
-@onready var collectible_item: CollectibleItem = %CollectibleItem
 
 
 func _ready() -> void:
-	DialogueManager.show_dialogue_balloon(intro_dialogue, "", [self])
-	await DialogueManager.dialogue_ended
-	# Add a short delay so the player doesn"t attack when closing the dialogue:
+	if intro_dialogue:
+		DialogueManager.show_dialogue_balloon(intro_dialogue, "", [self])
+		await DialogueManager.dialogue_ended
+
+	# Add a short delay so the player doesn"t attack when closing the dialogue
+	# or just entered the scene:
 	await get_tree().create_timer(0.5).timeout
+
 	fill_game_logic.goal_reached.connect(_on_goal_reached)
 	fill_game_logic.start()
 
 
 func _on_goal_reached() -> void:
-	collectible_item.reveal()
+	goal_reached.emit()
