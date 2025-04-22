b09ae65422972067427fac6dc24d4b5ba256d743
Juan Manuel Fernandes dos Santos <jfdossantos@frba.utn.edu.ar>
2025-04-22 14:57:09 -0300

    Change player attack from AnimatableBody to Area
    Player's attack was a static body whose size grows when attacking. This caused unexpected behavior when trying to use the attack near another static body: if a projectile was between the player and a wall, the attack could push the projectile into the wall because it's pushing the rigidbody around every frame.
    
    By having it by an area, we can make it so the attack only applies an impulse to the projectile on body entered, and if there's a wall it should just bounce off it.
    
    Also, the way it's implemented now, we can make things be 'attackable' by having them in the colliding mask of the player's attack + implementing a hit_by method in those objects. This could help make the world a bit more interactive later.

--- scenes/game_elements/characters/player/components/player_fighting.gd
+++ scenes/game_elements/characters/player/components/player_fighting.gd
@@ -6,10 +6,12 @@ var is_fighting: bool = false
 
 @onready var hit_box: Area2D = %HitBox
 @onready var got_hit_animation: AnimationPlayer = %GotHitAnimation
+@onready var air_stream: Area2D = %AirStream
 
 
 func _ready() -> void:
 	hit_box.body_entered.connect(_on_body_entered)
+	air_stream.body_entered.connect(_on_air_stream_body_entered)
 
 
 func _process(_delta: float) -> void:
@@ -26,3 +28,7 @@ func _on_body_entered(body: Node2D) -> void:
 	body.add_small_fx()
 	body.queue_free()
 	got_hit_animation.play(&"got_hit")
+
+
+func _on_air_stream_body_entered(body: Node2D) -> void:
+	body.hit_by(self)
