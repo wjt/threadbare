d79907bdc77f876ee186139964f8cec7e3764fc9
Will Thompson <wjt@endlessos.org>
2025-04-23 16:45:15 +0100

    Ink combat: Pause player input during intro dialogue
    Previously you could move the player while the opening dialogue was
    playing.
    
    Pause PLAYER_INPUT while the dialogue is playing.

--- scenes/quests/lore_quests/quest_001/2_ink_combat/components/ink_combat.gd
+++ scenes/quests/lore_quests/quest_001/2_ink_combat/components/ink_combat.gd
@@ -11,8 +11,10 @@ signal goal_reached
 
 func _ready() -> void:
 	if intro_dialogue:
+		Pause.pause_system(Pause.System.PLAYER_INPUT, self)
 		DialogueManager.show_dialogue_balloon(intro_dialogue, "", [self])
 		await DialogueManager.dialogue_ended
+		Pause.unpause_system(Pause.System.PLAYER_INPUT, self)
 
 	# Add a short delay so the player doesn"t attack when closing the dialogue
 	# or just entered the scene:
