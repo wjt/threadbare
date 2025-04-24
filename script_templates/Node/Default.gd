bfd399b6da5864645b0fb7f20f5a344f742d5d68
Juan Manuel Fernandes dos Santos <jfdossantos@frba.utn.edu.ar>
2025-04-24 14:59:10 -0300

    Add script template for default node with licensing header
    This is for those contributors like myself that forget to add the licensing info until CI rejects their PRs :P.

---
+++ script_templates/Node/Default.gd
@@ -0,0 +1,16 @@
+# meta-default: true
+# meta-description: Base template for Node with default Godot cycle methods. Includes Threadbare licensing header.
+# meta-name: Default (Threadbare)
+# SPDX-FileCopyrightText: The Threadbare Authors
+# SPDX-License-Identifier: MPL-2.0
+extends _BASE_
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass  # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
