42400072268730482287b2729181d7e56140310a
Will Thompson <wjt@endlessos.org>
2025-04-26 08:24:27 +0100

    Remove short timeouts after dialogue
    Previously, we had short delays after dialogue ended before enabling
    regular gameplay (interactions, movement, ink combat) again. This was to
    avoid the ui_accept action that ended the dialogue being handled a
    second time as the attack action or beginning another interaction with the same entity.
    
    Now that player input is handled in _unhandled_input(), this problem
    does not occur: Godot Dialogue Manager consumes all input during
    dialogue so there is no unhandled input left to be handled a second
    time.
    
    Remove the timeouts, and for good measure a call into the pause system.
    
    Fixes https://github.com/endlessm/threadbare/issues/103

--- scenes/quests/lore_quests/quest_001/2_ink_combat/components/ink_combat.gd
+++ scenes/quests/lore_quests/quest_001/2_ink_combat/components/ink_combat.gd
@@ -11,14 +11,8 @@ signal goal_reached
 
 func _ready() -> void:
 	if intro_dialogue:
-		Pause.pause_system(Pause.System.PLAYER_INPUT, self)
 		DialogueManager.show_dialogue_balloon(intro_dialogue, "", [self])
 		await DialogueManager.dialogue_ended
-		Pause.unpause_system(Pause.System.PLAYER_INPUT, self)
-
-	# Add a short delay so the player doesn"t attack when closing the dialogue
-	# or just entered the scene:
-	await get_tree().create_timer(0.5).timeout
 
 	fill_game_logic.goal_reached.connect(_on_goal_reached)
 	fill_game_logic.start()
