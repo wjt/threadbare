wjt/threadbare

commit 5e96011bb7430a161a133367f312c13b9868223f

Change player attack from AnimatableBody to Area
Player's attack was a static body whose size grows when attacking. This caused unexpected behavior when trying to use the attack near another static body: if a projectile was between the player and a wall, the attack could push the projectile into the wall because it's pushing the rigidbody around every frame.

By having it by an area, we can make it so the attack only applies an impulse to the projectile on body entered, and if there's a wall it should just bounce off it.

Also, the way it's implemented now, we can make things be 'attackable' by having them in the colliding mask of the player's attack + implementing a hit_by method in those objects. This could help make the world a bit more interactive later.

scenes/quests/lore_quests/quest_001/2_ink_combat/components/projectile/components/projectile.gd
@@ -105,9 +105,6 @@ func add_small_fx() -> void:
 func _on_body_entered(body: Node2D) -> void:
 	if body.owner is FillingBarrel and not can_hit_enemy:
 		return
-	var hit_vector: Vector2 = global_position - body.global_position
-	linear_velocity = Vector2.ZERO
-	apply_impulse(hit_vector)
 	add_small_fx()
 	duration_timer.start()
 	if body.owner is FillingBarrel:
@@ -115,9 +112,15 @@ func _on_body_entered(body: Node2D) -> void:
 		if filling_barrel.label == label:
 			filling_barrel.fill()
 			queue_free()
-	elif body.owner is Player:
-		can_hit_enemy = true
-		hit_sound.play()
+
+
+func hit_by(attack: Node2D) -> void:
+	var hit_speed := 100.0
+	var hit_vector: Vector2 = attack.global_position.direction_to(global_position) * hit_speed
+	can_hit_enemy = true
+	hit_sound.play()
+	linear_velocity = Vector2.ZERO
+	apply_impulse(hit_vector)
 
 
 func _on_duration_timer_timeout() -> void:

scenes/game_elements/characters/player/components/player_fighting.gd
@@ -6,10 +6,12 @@ var is_fighting: bool = false
 
 @onready var hit_box: Area2D = %HitBox
 @onready var got_hit_animation: AnimationPlayer = %GotHitAnimation
+@onready var air_stream: Area2D = %AirStream
 
 
 func _ready() -> void:
 	hit_box.body_entered.connect(_on_body_entered)
+	air_stream.body_entered.connect(_on_air_stream_body_entered)
 
 
 func _process(_delta: float) -> void:
@@ -26,3 +28,7 @@ func _on_body_entered(body: Node2D) -> void:
 	body.add_small_fx()
 	body.queue_free()
 	got_hit_animation.play(&"got_hit")
+
+
+func _on_air_stream_body_entered(body: Node2D) -> void:
+	body.hit_by(self)

scenes/game_elements/characters/player/player.tscn
@@ -1,4 +1,4 @@
-[gd_scene load_steps=27 format=3 uid="uid://iu2q66clupc6"]
+[gd_scene load_steps=26 format=3 uid="uid://iu2q66clupc6"]
 
 [ext_resource type="Script" uid="uid://bwllxup305eib" path="res://scenes/game_elements/characters/player/components/player.gd" id="1_g2els"]
 [ext_resource type="SpriteFrames" uid="uid://dtoylirwywk0j" path="res://scenes/game_elements/characters/shared_components/sprite_frames/story_weaver.tres" id="2_3in67"]
@@ -20,10 +20,6 @@ size = Vector2(110, 80)
 
 [sub_resource type="RectangleShape2D" id="RectangleShape2D_je7p5"]
 size = Vector2(32, 47)
-
-[sub_resource type="PhysicsMaterial" id="PhysicsMaterial_fm80t"]
-friction = 0.0
-bounce = 1000.0
 
 [sub_resource type="CircleShape2D" id="CircleShape2D_h17s1"]
 radius = 20.0
@@ -419,12 +415,11 @@ position = Vector2(0, -28.5)
 shape = SubResource("RectangleShape2D_je7p5")
 debug_color = Color(0.94902, 0.231373, 0, 0.419608)
 
-[node name="AirStream" type="AnimatableBody2D" parent="PlayerFighting"]
+[node name="AirStream" type="Area2D" parent="PlayerFighting"]
 unique_name_in_owner = true
 position = Vector2(0, -24)
-collision_layer = 1024
-collision_mask = 768
-physics_material_override = SubResource("PhysicsMaterial_fm80t")
+collision_layer = 0
+collision_mask = 256
 
 [node name="AirStreamCollision" type="CollisionShape2D" parent="PlayerFighting/AirStream"]
 unique_name_in_owner = true

scenes/quests/lore_quests/quest_001/2_ink_combat/components/projectile/projectile.tscn
@@ -7,7 +7,7 @@
 
 [sub_resource type="PhysicsMaterial" id="PhysicsMaterial_tbgi4"]
 friction = 0.0
-bounce = 1000.0
+bounce = 1.0
 
 [sub_resource type="Animation" id="Animation_l5glv"]
 length = 0.001
@@ -69,11 +69,12 @@ anim_speed_max = 1.0
 
 [node name="Projectile" type="RigidBody2D"]
 collision_layer = 256
-collision_mask = 576
+collision_mask = 80
 mass = 0.3
 physics_material_override = SubResource("PhysicsMaterial_tbgi4")
 gravity_scale = 0.0
 lock_rotation = true
+continuous_cd = 2
 contact_monitor = true
 max_contacts_reported = 1
 script = ExtResource("1_kxdg3")

commit 3dcb7d5ba51ac0bced73c002af5518accbd8f9d8

Projectile: Extend life when hit by player
This regressed in 5e96011b when changing the player attack from using a
physics body to an area. The life now has to be extended in the
`got_hit_by()` method.

scenes/quests/lore_quests/quest_001/2_ink_combat/components/projectile/components/projectile.gd
@@ -121,6 +121,8 @@ func _on_body_entered(body: Node2D) -> void:
 
 
 func got_hit(player: Player) -> void:
+	add_small_fx()
+	duration_timer.start()
 	var hit_speed := 100.0
 	var hit_vector: Vector2 = player.global_position.direction_to(global_position) * hit_speed
 	can_hit_enemy = true