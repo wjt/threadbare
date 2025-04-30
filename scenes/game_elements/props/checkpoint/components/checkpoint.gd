11d77ca18e41bcc0dc1403fbfe9ab48b408526c2
Will Thompson <wjt@endlessos.org>
2025-04-30 16:11:17 +0100

    Checkpoint: Add interaction
    This serves as a way to explain to the player what the checkpoint does, without
    completely breaking the forth wall.
    
    While trying to think of what the right verb might be, one of my favourite
    moments from Day of the Tentacle came to mind, and I couldn't resist adding an
    easter egg.

--- scenes/game_elements/props/checkpoint/components/checkpoint.gd
+++ scenes/game_elements/props/checkpoint/components/checkpoint.gd
@@ -4,6 +4,10 @@ class_name Checkpoint
 extends Area2D
 ## A place where the player respawns if the current scene is reloaded.
 
+const DIALOGUE: DialogueResource = preload(
+	"res://scenes/game_elements/props/checkpoint/components/checkpoint.dialogue"
+)
+
 ## The point where the player will spawn.
 @onready var spawn_point: SpawnPoint = %SpawnPoint
 
@@ -13,10 +17,13 @@ extends Area2D
 ## The collision shape for the sprite, when this checkpoint is activated
 @onready var collision_shape: CollisionShape2D = %CollisionShape
 
+@onready var interact_area: InteractArea = %InteractArea
+
 
 func _ready() -> void:
 	sprite.visible = false
 	body_entered.connect(func(_body: Node2D) -> void: self.activate())
+	interact_area.interaction_started.connect(_on_interaction_started)
 
 
 ## Makes this the active checkpoint.
@@ -24,3 +31,10 @@ func activate() -> void:
 	GameState.current_spawn_point = owner.get_path_to(spawn_point)
 	sprite.visible = true
 	collision_shape.set_deferred(&"disabled", false)
+	interact_area.disabled = false
+
+
+func _on_interaction_started(player: Player, _from_right: bool) -> void:
+	DialogueManager.show_dialogue_balloon(DIALOGUE, "", [self, player])
+	await DialogueManager.dialogue_ended
+	interact_area.interaction_ended.emit()
