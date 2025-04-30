1c22bb7dc0cf3b63152ff6a1ef9553dc1686e6c7
Will Thompson <wjt@endlessos.org>
2025-04-30 11:09:04 +0100

    Title: Suppress pause action
    Previously, the title screen could be paused. Handle the pause action as a no-op
    so that it cannot be.

--- scenes/menus/title/components/title_screen.gd
+++ scenes/menus/title/components/title_screen.gd
@@ -8,6 +8,11 @@ extends Control
 @onready var credits: Control = %Credits
 
 
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed(&"pause"):
+		get_viewport().set_input_as_handled()
+
+
 func _on_start_pressed() -> void:
 	(
 		SceneSwitcher
