2dedba48be4249be5b62008e94cbb6ebc7a8c549
Will Thompson <wjt@endlessos.org>
2025-04-30 15:44:42 +0100

    Indicate checkpoints visually
    Previously, there was no visible indication that there are checkpoints in the
    stealth level, or that you have reached one. In playtesting, some players
    suggested that we should add checkpoints to the stealth level, not realising
    that they were already present.
    
    Add a sprite to the checkpoint scene. Hide it until the player enters the
    checkpoint area.
    
    Add a small collision shape to it, so that the player can't walk through it.
    Deactivate that collision shape when the sprite is invisible.
    
    Enable y-sorting on the parent node of all the checkpoints in the two stealth
    levels currently in the source tree.
    
    For now, a scarecrow is used to indicate the checkpoint, as a placeholder. I
    have not added any kind of animation or sound effect when the checkpoint is
    reached.
    
    Fixes https://github.com/endlessm/threadbare/issues/240

--- scenes/game_elements/props/checkpoint/components/checkpoint.gd
+++ scenes/game_elements/props/checkpoint/components/checkpoint.gd
@@ -1,17 +1,26 @@
 # SPDX-FileCopyrightText: The Threadbare Authors
 # SPDX-License-Identifier: MPL-2.0
-class_name Checkpoint extends Area2D
-
+class_name Checkpoint
+extends Area2D
 ## A place where the player respawns if the current scene is reloaded.
 
 ## The point where the player will spawn.
 @onready var spawn_point: SpawnPoint = %SpawnPoint
 
+## The sprite displayed when this checkpoint is activated
+@onready var sprite: Sprite2D = %Sprite
+
+## The collision shape for the sprite, when this checkpoint is activated
+@onready var collision_shape: CollisionShape2D = %CollisionShape
+
 
 func _ready() -> void:
+	sprite.visible = false
 	body_entered.connect(func(_body: Node2D) -> void: self.activate())
 
 
 ## Makes this the active checkpoint.
 func activate() -> void:
 	GameState.current_spawn_point = owner.get_path_to(spawn_point)
+	sprite.visible = true
+	collision_shape.set_deferred(&"disabled", false)
