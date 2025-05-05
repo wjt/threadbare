8d95eda6c551929221639f9d6133e8061c6b0620
Manuel Quiñones <manuel.por.aca@gmail.com>
2025-05-05 09:39:52 -0300

    Stealth game: Group guards
    Instead of iterating the children of a specific node, add guards to a
    new global group named "guard_enemy". Just like the existing
    "throwing_enemy". And pick all nodes in that group to connect them with
    the player detection.

--- scenes/quests/lore_quests/quest_001/3_stealth_level/components/stealth_quest.gd
+++ scenes/quests/lore_quests/quest_001/3_stealth_level/components/stealth_quest.gd
@@ -1,16 +1,13 @@
 # SPDX-FileCopyrightText: The Threadbare Authors
 # SPDX-License-Identifier: MPL-2.0
 extends Node2D
-@onready var enemy_guards: Node2D = $EnemyGuards
 @onready var transition_fade: ColorRect = %TransitionFade
 
 
 func _ready() -> void:
 	await create_tween().tween_property(transition_fade, "color:a", 0.0, 0.5).from(1.0).finished
-	for node: Node in enemy_guards.get_children():
-		var guard := node as Guard
-		if guard:
-			guard.player_detected.connect(self.on_player_detected)
+	for guard: Guard in get_tree().get_nodes_in_group(&"guard_enemy"):
+		guard.player_detected.connect(self.on_player_detected)
 
 
 func on_player_detected(player: Node2D) -> void:
