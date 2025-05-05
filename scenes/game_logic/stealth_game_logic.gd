18bc3c5c96126ed7a2730311d6889ce97c867574
Manuel Quiñones <manuel.por.aca@gmail.com>
2025-05-05 11:13:26 -0300

    Stealth game: Join logic in a single script
    And move it from the Lore quest folder to the scenes/game_logic folder.
    
    The logic was split in 2 scripts. Merge them so we have a single node
    with the logic.
    
    Also use the naming convention of starting functions with an underscore
    in the private methods of the script.
    
    Add a class name to the script and add it as a node to the Stealth
    scenes.

--- scenes/game_logic/stealth_game_logic.gd
+++ scenes/game_logic/stealth_game_logic.gd
@@ -1,22 +1,31 @@
 # SPDX-FileCopyrightText: The Threadbare Authors
 # SPDX-License-Identifier: MPL-2.0
 @tool
+class_name StealthGameLogic
 extends Node
 
 @export var player_instantly_loses_on_sight: bool = false:
-	set = set_player_instantly_loses_on_sight
+	set = _set_player_instantly_loses_on_sight
 @export_range(0.5, 3.0, 0.1, "or_greater", "or_less") var zoom: float = 1.0:
-	set = set_zoom
+	set = _set_zoom
 
 
 func _ready() -> void:
 	if Engine.is_editor_hint():
 		return
-	set_player_instantly_loses_on_sight(player_instantly_loses_on_sight)
-	set_zoom(zoom)
+	_set_player_instantly_loses_on_sight(player_instantly_loses_on_sight)
+	_set_zoom(zoom)
+	for guard: Guard in get_tree().get_nodes_in_group(&"guard_enemy"):
+		guard.player_detected.connect(self._on_player_detected)
 
 
-func set_player_instantly_loses_on_sight(new_value: bool) -> void:
+func _on_player_detected(player: Node2D) -> void:
+	player.process_mode = ProcessMode.PROCESS_MODE_DISABLED
+	await get_tree().create_timer(2.0).timeout
+	SceneSwitcher.reload_with_transition(Transition.Effect.FADE, Transition.Effect.FADE)
+
+
+func _set_player_instantly_loses_on_sight(new_value: bool) -> void:
 	player_instantly_loses_on_sight = new_value
 	if not is_node_ready():
 		return
@@ -24,7 +33,7 @@ func set_player_instantly_loses_on_sight(new_value: bool) -> void:
 		guard.player_instantly_detected_on_sight = player_instantly_loses_on_sight
 
 
-func set_zoom(new_value: float) -> void:
+func _set_zoom(new_value: float) -> void:
 	zoom = new_value
 	if Engine.is_editor_hint():
 		return
