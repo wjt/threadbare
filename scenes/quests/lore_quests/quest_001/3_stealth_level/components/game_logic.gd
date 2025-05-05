b2fbb4ad65e71f9ad1ee0924425485c7356497e2
Manuel Quiñones <manuel.por.aca@gmail.com>
2025-05-05 10:46:27 -0300

    Stealth game logic: Reference guards by group
    Also revoke unique name from guards node. This is not needed anymore as
    now guards are referenced by group.

--- scenes/quests/lore_quests/quest_001/3_stealth_level/components/game_logic.gd
+++ scenes/quests/lore_quests/quest_001/3_stealth_level/components/game_logic.gd
@@ -8,18 +8,14 @@ extends Node
 @export_range(0.5, 3.0, 0.1, "or_greater", "or_less") var zoom: float = 1.0:
 	set = set_zoom
 
-@onready var enemy_guards: Node2D = %EnemyGuards
 @onready var camera_2d: Camera2D = %Camera2D
 
 
 func set_player_instantly_loses_on_sight(new_value: bool) -> void:
 	player_instantly_loses_on_sight = new_value
-	if not enemy_guards:
+	if not is_node_ready():
 		return
-	for child: Node in enemy_guards.get_children():
-		var guard := child as Guard
-		if not guard:
-			continue
+	for guard: Guard in get_tree().get_nodes_in_group(&"guard_enemy"):
 		guard.player_instantly_detected_on_sight = player_instantly_loses_on_sight
 
 
