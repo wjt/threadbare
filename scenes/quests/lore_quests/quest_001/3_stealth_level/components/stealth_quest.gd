9d17fbe9263881477942baae94dfab28990f7b46
Manuel Quiñones <manuel.por.aca@gmail.com>
2025-05-05 10:39:12 -0300

    Stealth quest & template: Remove custom transition
    In the Stealth of Lore quest there are 2 transitions happening when the
    scene loads: one triggered by the collectible using SceneSwitcher, and a
    custom one. The custom transition also has a %TransitionFade node as a
    dependency in the script.
    
    Remove custom fade-in transition from the Stealth script. One is enough.
    
    Remove custom fade-out transition when the player is detected. Use the
    new SceneSwitcher.reload_with_transition instead.
    
    Remove the corresponding nodes from Lore and template Stealth scenes.
    
    Note: This changes the transition from half a second to one second. We
    can add a parameter all the way down to the tween, but it doesn't seem
    worth for now.

--- scenes/quests/lore_quests/quest_001/3_stealth_level/components/stealth_quest.gd
+++ scenes/quests/lore_quests/quest_001/3_stealth_level/components/stealth_quest.gd
@@ -1,11 +1,9 @@
 # SPDX-FileCopyrightText: The Threadbare Authors
 # SPDX-License-Identifier: MPL-2.0
 extends Node2D
-@onready var transition_fade: ColorRect = %TransitionFade
 
 
 func _ready() -> void:
-	await create_tween().tween_property(transition_fade, "color:a", 0.0, 0.5).from(1.0).finished
 	for guard: Guard in get_tree().get_nodes_in_group(&"guard_enemy"):
 		guard.player_detected.connect(self.on_player_detected)
 
@@ -13,5 +11,4 @@ func _ready() -> void:
 func on_player_detected(player: Node2D) -> void:
 	player.process_mode = ProcessMode.PROCESS_MODE_DISABLED
 	await get_tree().create_timer(2.0).timeout
-	await create_tween().tween_property(transition_fade, "color:a", 1.0, 0.5).finished
-	get_tree().reload_current_scene()
+	SceneSwitcher.reload_with_transition(Transition.Effect.FADE, Transition.Effect.FADE)
