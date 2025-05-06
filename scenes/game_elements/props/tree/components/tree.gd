d403b4421245c3ddfa221e9d86073d3a58cbdd45
Manuel Quiñones <manuel.por.aca@gmail.com>
2025-05-06 11:51:51 -0300

    Tree: Export internal sprite frames
    So it can be changed per tree.
    
    Save current green sprite frames in a resource file, and reference it as
    the default through its UID.
    
    Also set the animation to 10 frames per second, which is how all
    animations are meant to be.
    
    Also call play(), when setting the sprite frames. This is needed when
    the aprite frames change. It also
    makes the frame-by-frame animation be played in the editor.

--- scenes/game_elements/props/tree/components/tree.gd
+++ scenes/game_elements/props/tree/components/tree.gd
@@ -3,10 +3,26 @@
 @tool
 extends Decoration
 
+const DEFAULT_SPRITE_FRAME: SpriteFrames = preload("uid://d36eq8tqdaxdy")
+
+@export var sprite_frames: SpriteFrames = DEFAULT_SPRITE_FRAME:
+	set = _set_sprite_frames
+
 @onready var animated_sprite_2d: AnimatedSprite2D = %AnimatedSprite2D
 
 
+func _set_sprite_frames(new_sprite_frames: SpriteFrames) -> void:
+	sprite_frames = new_sprite_frames
+	if not is_node_ready():
+		return
+	if new_sprite_frames == null:
+		new_sprite_frames = DEFAULT_SPRITE_FRAME
+	animated_sprite_2d.sprite_frames = new_sprite_frames
+	animated_sprite_2d.play(animated_sprite_2d.animation)
+
+
 func _ready() -> void:
+	_set_sprite_frames(sprite_frames)
 	var frames_length: int = animated_sprite_2d.sprite_frames.get_frame_count(
 		animated_sprite_2d.animation
 	)
