703e796eed5738cfb9b8e0a2ec59bb7cba3e53ef
Will Thompson <wjt@endlessos.org>
2025-05-08 20:04:45 +0100

    Hud: Fix shadowing warning
    W 0:00:00:879   The local function parameter "is_visible" is shadowing an already-declared method in the base class "CanvasLayer".
      <GDScript Error>SHADOWED_VARIABLE_BASE_CLASS
      <GDScript Source>hud.gd:8

--- scenes/ui_elements/hud/components/hud.gd
+++ scenes/ui_elements/hud/components/hud.gd
@@ -5,5 +5,5 @@ extends CanvasLayer
 @onready var story_quest_progress: PanelContainer = %StoryQuestProgress
 
 
-func change_story_quest_progress_visibility(is_visible: bool) -> void:
-	story_quest_progress.visible = is_visible
+func change_story_quest_progress_visibility(visibility: bool) -> void:
+	story_quest_progress.visible = visibility
