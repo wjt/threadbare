b0dfc12801f98f0d5dcf9d846aab0e7f9307e5f8
Will Thompson <wjt@endlessos.org>
2025-05-09 13:59:05 +0100

    MusicalRock: Allow customising sprite frames
    This follows a similar pattern used elsewhere in the project, including
    adding a warning if the expected animations are not defined.
    
    This is primarily intended for StoryQuests but you could imagine we
    could also have different-coloured versions of the base rock asset,
    rather than using the modulate property as we currently do.
    
    Fixes https://github.com/endlessm/threadbare/issues/524

--- scenes/quests/lore_quests/quest_001/1_music_puzzle/components/xylophone/musical_rock.gd
+++ scenes/quests/lore_quests/quest_001/1_music_puzzle/components/xylophone/musical_rock.gd
@@ -1,10 +1,23 @@
 # SPDX-FileCopyrightText: The Threadbare Authors
 # SPDX-License-Identifier: MPL-2.0
+@tool
 class_name MusicalRock
 extends StaticBody2D
 
 ## Emitted when the rock is struck by the player.
 signal note_played
+
+const DEFAULT_SPRITE_FRAMES: SpriteFrames = preload("uid://7ul4p7v1ve0p")
+
+## The animations which must be defined for [member sprite_frames]. The [code]default[/code]
+## animation is used when the object is idle; [code]struck[/code] is used when the player interacts
+## with the object.
+const REQUIRED_ANIMATIONS: Array[StringName] = [&"default", &"struck"]
+
+## Animations for this object. The SpriteFrames must have specific animations.
+## See [constant MusicalRock.REQUIRED_ANIMATIONS].
+@export var sprite_frames: SpriteFrames = DEFAULT_SPRITE_FRAMES:
+	set = _set_sprite_frames
 
 @export var audio_stream: AudioStream
 
@@ -13,7 +26,26 @@ signal note_played
 @onready var audio_stream_player_2d: AudioStreamPlayer2D = %AudioStreamPlayer2D
 
 
+func _set_sprite_frames(new_sprite_frames: SpriteFrames) -> void:
+	sprite_frames = new_sprite_frames
+	if not is_node_ready():
+		return
+	if new_sprite_frames == null:
+		new_sprite_frames = DEFAULT_SPRITE_FRAMES
+	animated_sprite.sprite_frames = new_sprite_frames
+	update_configuration_warnings()
+
+
+func _get_configuration_warnings() -> PackedStringArray:
+	var warnings: Array = []
+	for animation in REQUIRED_ANIMATIONS:
+		if not sprite_frames.has_animation(animation):
+			warnings.append("sprite_frames is missing the following animation: %s" % animation)
+	return warnings
+
+
 func _ready() -> void:
+	_set_sprite_frames(sprite_frames)
 	audio_stream_player_2d.stream = audio_stream
 
 
