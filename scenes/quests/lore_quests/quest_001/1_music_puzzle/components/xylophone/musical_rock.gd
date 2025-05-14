f8916a956fa4a43f98221d1b5b9739dd1ae8baab
Will Thompson <wjt@endlessos.org>
2025-05-14 14:19:18 +0100

    MusicalRock: Wobble for a fixed duration
    Previously, the play() method would resolve after:
    
    - The rock's audio stream has played to completion; then
    - The current loop of the struck animation has finished.
    
    This meant that, when a sequence is being demoed, if the audio streams
    for each rock are different lengths (which they are – quite markedly so)
    then the demo does not play at a consistent speed. This looks & sounds
    bad, and will also makes it hard to synchronise a hint animation on the
    sign that the player has interacted with to play the demo.
    
    Make the play() method (which is async) take a fixed length of time,
    configured via a property which defaults to 1 second. Update the
    wobble_silently() implementation (which was already fixed at 1 second)
    to run for the same time, using the same logic.

--- scenes/quests/lore_quests/quest_001/1_music_puzzle/components/xylophone/musical_rock.gd
+++ scenes/quests/lore_quests/quest_001/1_music_puzzle/components/xylophone/musical_rock.gd
@@ -11,7 +11,7 @@ const DEFAULT_SPRITE_FRAMES: SpriteFrames = preload("uid://7ul4p7v1ve0p")
 
 ## The animations which must be defined for [member sprite_frames]. The [code]default[/code]
 ## animation is used when the object is idle; [code]struck[/code] is used when the player interacts
-## with the object.
+## with the object. Both animations should loop cleanly.
 const REQUIRED_ANIMATIONS: Array[StringName] = [&"default", &"struck"]
 
 ## Animations for this object. The SpriteFrames must have specific animations.
@@ -19,7 +19,15 @@ const REQUIRED_ANIMATIONS: Array[StringName] = [&"default", &"struck"]
 @export var sprite_frames: SpriteFrames = DEFAULT_SPRITE_FRAMES:
 	set = _set_sprite_frames
 
+## Sound to play when the player strikes this object.
 @export var audio_stream: AudioStream
+
+## How long the [code]struck[/code] animation should play for when the object is struck by the
+## player. This should typically be similar to the duration of [member audio_stream]. The player
+## can interact with the object again even before this time has elapsed, but demonstrations of the
+## sequence will wait for exactly this time before continuing to the next object.
+@export_range(0.5, 1.5, 1.0 / 60, "or_less", "or_greater", "suffix:s")
+var strike_duration: float = 1.0
 
 @onready var animated_sprite: AnimatedSprite2D = %AnimatedSprite2D
 @onready var interact_area: InteractArea = %InteractArea
@@ -56,21 +64,28 @@ func _on_interaction_started(_player: Player, _from_right: bool) -> void:
 
 
 ## Act as if the rock was struck by the player. Does not emit [member note_played].
-func play() -> void:
+func play(silent: bool = false) -> void:
 	animated_sprite.play(&"struck")
-	audio_stream_player_2d.play()
-	await audio_stream_player_2d.finished
-	await animated_sprite.animation_looped
-	animated_sprite.play(&"default")
+	if not silent:
+		audio_stream_player_2d.play()
+
+	await get_tree().create_timer(strike_duration).timeout
+
+	# Don't wait for the end of the animation loop, so that this method takes exactly
+	# strike_duration seconds to resolved and a demo can be synced to an animation.
+	_stop()
 
 
-func wobble_silently() -> void:
-	animated_sprite.play(&"struck")
-	await get_tree().create_timer(1.0).timeout
-	stop_hint()
-
-
-func stop_hint() -> void:
+func _stop() -> void:
 	if animated_sprite.is_playing() and animated_sprite.animation == "struck":
 		await animated_sprite.animation_looped
 		animated_sprite.play("default")
+
+
+func wobble_silently() -> void:
+	await play(true)
+
+
+## Stop the silent hint animation, if playing.
+func stop_hint() -> void:
+	await _stop()
