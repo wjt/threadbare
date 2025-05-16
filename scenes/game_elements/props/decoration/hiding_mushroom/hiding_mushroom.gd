3f5452a33bbf12c6cd0cb927bc6a2b699566c313
Will Thompson <wjt@endlessos.org>
2025-05-16 11:12:52 +0100

    hiding_mushroom: Simplify behaviour
    Previously, the behaviour of the mushroom was configurable: it could be
    configured to start hidden, or start visible; and it could be configured
    to stay hidden/revealed once its state changes. I found the logic
    confusing, particularly the dynamic redefinition of the "stay
    hidden/revealed" property based on the value of is_hidden. And the
    behaviour of the four instances in Fray's End was incorrect: they are
    supposed to start revealed, and hide when the player gets near.
    
    Remove all configuration from the mushroom. Make it always follow the
    behaviour of being visible unless the player is nearby.
    
    Fixes https://github.com/endlessm/threadbare/issues/580

--- scenes/game_elements/props/decoration/hiding_mushroom/hiding_mushroom.gd
+++ scenes/game_elements/props/decoration/hiding_mushroom/hiding_mushroom.gd
@@ -3,21 +3,6 @@
 @tool
 extends Node2D
 
-@export var is_hidden: bool = true:
-	set(val):
-		var was_hidden: bool = is_hidden
-		is_hidden = val
-		if not is_node_ready():
-			return
-		if is_hidden != was_hidden:
-			_update_hidden_state()
-			notify_property_list_changed()
-
-@export var stay_hidden_or_revealed: bool = false
-
-var _reveal_when_player_nearby: bool = false
-var _hide_when_player_nearby: bool = false
-
 @onready var animated_sprite_2d: AnimatedSprite2D = %AnimatedSprite2D
 
 
@@ -36,40 +21,11 @@ func _reveal() -> void:
 		animated_sprite_2d.play(&"idle")
 
 
-func _update_hidden_state() -> void:
-	if is_hidden:
-		_hide()
-	else:
-		_reveal()
-
-
-func _get_property_list() -> Array[Dictionary]:
-	return [
-		{
-			"name": "_reveal_when_player_nearby" if is_hidden else "_hide_when_player_nearby",
-			"type": TYPE_BOOL,
-			"usage": PROPERTY_USAGE_DEFAULT,
-		}
-	]
-
-
-func _reveal_or_hide(is_player_nearby: bool) -> void:
-	if _reveal_when_player_nearby:
-		is_hidden = not is_player_nearby
-	elif _hide_when_player_nearby:
-		is_hidden = is_player_nearby
-
-
-func _ready() -> void:
-	if is_hidden:
-		animated_sprite_2d.visible = false
-
-
 func _on_player_detector_body_entered(body: Node2D) -> void:
 	if body.is_in_group(&"player"):
-		_reveal_or_hide(true)
+		_hide()
 
 
 func _on_player_detector_body_exited(body: Node2D) -> void:
-	if body.is_in_group(&"player") and not stay_hidden_or_revealed:
-		_reveal_or_hide(false)
+	if body.is_in_group(&"player"):
+		_reveal()
