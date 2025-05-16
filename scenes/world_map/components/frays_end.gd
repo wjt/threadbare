1a6bfe20c9f26223ea9dac9f8e06f9e0f77e85bb
Will Thompson <wjt@endlessos.org>
2025-05-16 10:57:55 +0100

    frays_end: Fix static typing warnings

--- scenes/world_map/components/frays_end.gd
+++ scenes/world_map/components/frays_end.gd
@@ -6,12 +6,10 @@ extends Node2D
 @onready var eternal_loom: EternalLoom = %EternalLoom
 
 
-func _ready():
+func _ready() -> void:
 	_update_story_quest_progress_visibility()
-	GameState.collected_items_changed.connect(
-		func(_new_items): _update_story_quest_progress_visibility()
-	)
+	GameState.collected_items_changed.connect(_update_story_quest_progress_visibility)
 
 
-func _update_story_quest_progress_visibility():
+func _update_story_quest_progress_visibility(_new_items: Array[InventoryItem] = []) -> void:
 	hud.change_story_quest_progress_visibility(eternal_loom.is_item_offering_possible())
