fff08c5fc76899fe6b7ed17c975aca6d808236f5
Manuel Quiñones <manuel.por.aca@gmail.com>
2025-05-17 00:05:45 -0300

    Stealth game: Change player to defeated when cought
    Previously the whole player was set to process mode disabled.

--- scenes/game_logic/stealth_game_logic.gd
+++ scenes/game_logic/stealth_game_logic.gd
@@ -17,7 +17,7 @@ func _ready() -> void:
 
 
 func _on_player_detected(player: Node2D) -> void:
-	player.process_mode = ProcessMode.PROCESS_MODE_DISABLED
+	player.mode = Player.Mode.DEFEATED
 	await get_tree().create_timer(2.0).timeout
 	SceneSwitcher.reload_with_transition(Transition.Effect.FADE, Transition.Effect.FADE)
 
