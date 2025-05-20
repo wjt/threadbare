8e894af68bee2b8bed7bcc446473c43fb97b0afa
Will Thompson <wjt@endlessos.org>
2025-05-20 17:25:45 +0100

    Elder: Offer the player a storybook
    Previously, each elder could trigger exactly one quest. Soon we will
    have more than one StoryQuest, and in due course more than one
    LoreQuest, so we need a way to access those.
    
    Wire up the existing Storybook scene to the elder. Trigger its display
    from dialogue, hiding the dialogue balloon while the storybook is
    on-screen. This replaces the previous yes/no choice in each dialogue
    (and may address https://github.com/endlessm/threadbare/issues/575).
    
    Fixes https://github.com/endlessm/threadbare/issues/164

--- scenes/game_elements/characters/npcs/elder/components/elder.gd
+++ scenes/game_elements/characters/npcs/elder/components/elder.gd
@@ -4,11 +4,10 @@
 class_name Elder
 extends Talker
 
-## The quest that this NPC offers to the player when they interact with them.
-@export var quest: Quest:
-	set(new_value):
-		quest = new_value
-		update_configuration_warnings()
+const STORYBOOK_SCENE := preload("uid://bhm7fdjvppt8b")
+
+## Directory of quests that this NPC offers to the player during interactions.
+@export_dir var quest_directory: String
 
 ## A reference to the loom, so that this Elder can determine whether you have
 ## the items you need to operate it.
@@ -22,22 +21,30 @@ extends Talker
 @export var idle_sound_stream: AudioStream = preload("uid://dxbxx6x5h7d8p"):
 	set = _set_idle_sound_stream
 
-## Whether to enter [member quest] when the current dialogue ends
-var _enter_quest_on_dialogue_ended: bool = false
+## The quest chosen by the player from the storybook
+var chosen_quest: Quest
+
+var _dialogue_balloon: CanvasLayer
+var _storybook: Storybook
 
 @onready var _book_sound: AudioStreamPlayer2D = %BookSound
+@onready var _storybook_layer: CanvasLayer = %StorybookLayer
 
 
 func _ready() -> void:
 	super._ready()
 	animated_sprite_2d.connect("frame_changed", _on_frame_changed)
 
+	if quest_directory:
+		_storybook = STORYBOOK_SCENE.instantiate()
+		_storybook.quest_directory = quest_directory
+
 
 func _get_configuration_warnings() -> PackedStringArray:
 	var warnings: Array[String] = []
 
-	if not quest:
-		warnings.append("Quest property should be set")
+	if not quest_directory:
+		warnings.append("Quest Directory property should be set")
 
 	if not eternal_loom:
 		warnings.append("Eternal Loom property must be set")
@@ -51,25 +58,35 @@ func _on_interaction_started(player: Player, _from_right: bool) -> void:
 	var title: String = ""
 	if eternal_loom and eternal_loom.is_item_offering_possible():
 		title = "go_to_loom"
-	DialogueManager.show_dialogue_balloon(dialogue, title, [self, player])
+	_dialogue_balloon = DialogueManager.show_dialogue_balloon(dialogue, title, [self, player])
 
 
-## At the end of the current interaction, enter [member quest]. This is intended to be called
-## from dialogue.
-func enter_quest() -> void:
-	_enter_quest_on_dialogue_ended = true
+## Show a storybook to the player, and wait for them to select a story or close the book.
+func show_storybook() -> void:
+	if not _storybook:
+		return
+
+	# GDM will hide the balloon after a short pause if the awaitable hasn't resolved, but we want it
+	# to be replaced with the storybook immediately.
+	if _dialogue_balloon:
+		_dialogue_balloon.balloon.hide()
+
+	_storybook_layer.add_child(_storybook)
+	_storybook.reset_focus()
+	chosen_quest = await _storybook.selected
+	_storybook_layer.remove_child(_storybook)
 
 
 func _on_dialogue_ended(dialogue_resource: DialogueResource) -> void:
 	super(dialogue_resource)
 
-	if _enter_quest_on_dialogue_ended:
+	if chosen_quest:
 		%InteractArea.disabled = true
 		GameState.start_quest()
 		SceneSwitcher.change_to_file_with_transition(
-			quest.first_scene, ^"", Transition.Effect.FADE, Transition.Effect.FADE
+			chosen_quest.first_scene, ^"", Transition.Effect.FADE, Transition.Effect.FADE
 		)
-		_enter_quest_on_dialogue_ended = false
+		chosen_quest = null
 
 
 func _on_frame_changed() -> void:
