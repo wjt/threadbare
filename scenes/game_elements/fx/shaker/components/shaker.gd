1adbcd6fb3a965488606c9a95b31acb06433dfb9
Luis David Escobedo Velasquez <lescobedo.im@gmail.com>
2025-09-04 12:01:09 -0500

    Shake component: Consider that the target object could be freed (#1134)
    Add validations to avoid crashing the game through a freed instance
    error (a Camera2D instance called 'target').
    
    Fix https://github.com/endlessm/threadbare/issues/934

--- scenes/game_elements/fx/shaker/components/shaker.gd
+++ scenes/game_elements/fx/shaker/components/shaker.gd
@@ -63,6 +63,7 @@ func _ready() -> void:
 ## Emits the [signal started] signal as soon as it's called. [br]
 ## If the shake is called multiple times, it will only emit the [signal
 ## finished] signal when the last effect is completed.
+## After the shake happens the target could have been freed, so consider that to avoid an error.
 func shake(intensity: float = shake_intensity, time: float = duration) -> void:
 	noise.seed = randi()
 	started.emit()
@@ -81,8 +82,13 @@ func shake(intensity: float = shake_intensity, time: float = duration) -> void:
 	time_passed = 0.0
 	await shake_tween.tween_property(self, "current_intensity", 0.0, time).from(intensity).finished
 	shake_tween.kill()
-	target.position = Vector2(original_position.x, original_position.y)
-	rotation = original_rotation
+
+	if is_instance_valid(target):
+		if target is Camera2D:
+			target.offset = Vector2(original_position.x, original_position.y)
+		else:
+			target.position = Vector2(original_position.x, original_position.y)
+		target.rotation = original_rotation
 	finished.emit()
 
 
@@ -94,7 +100,7 @@ func _input(event: InputEvent) -> void:
 
 
 func _process(delta: float) -> void:
-	if current_intensity > 0.0:
+	if current_intensity > 0.0 and is_instance_valid(target):
 		time_passed += delta * frequency
 		var offset_x: float = noise.get_noise_1d(time_passed) * current_intensity
 		var offset_y: float = noise.get_noise_1d(time_passed + 100) * current_intensity
@@ -104,6 +110,7 @@ func _process(delta: float) -> void:
 
 		var new_position := Vector2(original_position.x + offset_x, original_position.y + offset_y)
 		var new_rotation := original_rotation + rotation_offset
+
 		if target is Camera2D:
 			target.offset = new_position
 		else:
